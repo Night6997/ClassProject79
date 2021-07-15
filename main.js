84217f55965c8e53e6fed5ea6572f9ebb1af368e
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-07-15 20:50:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 NOS = [];
 function submit() {
-var Name1 = document.getElementById("name1").value;
-var Name2 = document.getElementById("name2").value;
-var Name3 = document.getElementById("name3").value;
-var Name4 = document.getElementById("name4").value;
-
-NOS.push(Name1);
-NOS.push(Name2);
-NOS.push(Name3);
-NOS.push(Name4);
-
+var nots = [];
+for(var i = 1; i<=4; i++) {
+var display_student_name = document.getElementById("name"+i).value;
+console.log(display_student_name);
+NOS.push(display_student_name);
+}
 console.log(NOS);
-document.getElementById("outputDiv").innerHTML = NOS;
-
-document.getElementById("submitButton").style.display="none";
-document.getElementById("sortButton").style.display="inline-block";
+var length_of_NOS = NOS.length;
+console.log(length_of_NOS);
+for(var o = 0; o < length_of_NOS; o++) {
+nots.push("<h4>Name- "+ NOS[o] + "</h4>")
+console.log(nots);
+}
+console.log(nots);
+document.getElementById("WithC").innerHTML = nots;
+var NoCNos = nots.join(" ");
+console.log(NoCNos);
+document.getElementById("WithoutC").innerHTML = NoCNos;
+document.getElementById("submitButton").style.display = "none";
+document.getElementById("sortButton").style.display = "inline-block";
 }
 function sort() {
-NOS.sort();
-console.log(NOS);
-document.getElementById("outputDiv").innerHTML = NOS;
+    NOS.sort();
+    console.log(NOS);
+    var sortedNOS = [];
+    var length_of_NOS = NOS.length;
+    console.log(length_of_NOS);
+    for(var o = 0; o < length_of_NOS; o++) {
+        sortedNOS.push("<h4>Name- "+NOS[o] + "</h4>");
+        console.log(sortedNOS);
+       }
+       var NoCNos = sortedNOS.join(" ");
+       console.log(NoCNos);
+       document.getElementById("WithoutC").innerHTML = NoCNos;
+}
+function newUpdate(){
+    document.getElementById("WithoutC").innerHTML = "<h1>" + NOS + "</h1>";
 }
